1b851e5b7c6a5687c9aa7ba59f854d09a6f6ef68
Boris Petrov <boris.petrov1214@gmail.com>
2014-08-29 08:53:04 +0300

    threadsafe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.threadsafe! unless ENV['THREADSAFE'] == 'off'
-
 end
